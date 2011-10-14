c6759251d2b0c32ea937a78a0c237af76d719e3d
John Schulz <jfsiii@JFSIII.local>
2011-10-14 07:05:26 -0400

    Added basic templating

--- app.js
+++ app.js
@@ -1,53 +1,87 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
+// score is 1 - (your_id / total_users)
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs')
     port = process.argv[2] || 32372;
 
 http.createServer(function(request, response) {
-    
-    var id, ext = '.html';
-    switch(request.url){
-    case '/':
-        id = 'front-page'; break;
-    default:
-        id = 'unknown-page'; break;
+    var parsed = url.parse(request.url), id;
+    //console.log('URL: ', request.url, parsed);
+
+    if (request.url === '/') {
+        id = 'front-page';
+    }
+    else if (request.url === '/favicon.ico') {
+        id = null;
+    }
+    else {
+        id = 'unknown-page';
     }
 
-    var pathname = path.join(process.cwd(), 'views', id + ext);
-
-    serveStatic(pathname, request, response);
+    var query = getQueryStringArgs(parsed.query);
+    var args = query || {};
+    args.foo = 'FOO'; args.bar = 'bAr'; args.baz = 'BAZ';
+    serveStatic(id, query, request, response);
 
 }).listen(parseInt(port, 10));
 
-function serveStatic(pathname, request, response){
+function serveStatic(id, args, request, response)
+{
+    var ext = '.html';
+    var pathname = path.join(process.cwd(), 'views', id + ext);
 
-    path.exists(pathname, function(exists) {
-        if(!exists) {
-            response.writeHead(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
+    path.exists(pathname, function (exists) {
+        if (!exists) {
+            response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.write('404 Not Found\n');
             response.end();
             return;
         }
-        
+
         if (fs.statSync(pathname).isDirectory()) pathname += '/index.html';
 
-        getFile(pathname, request, response)
+        getFile(pathname, args, request, response)
     });
 }
 
-function getFile(pathname, request, response){
-    fs.readFile(pathname, "binary", function(err, file) {
-        if(err) {        
-            response.writeHead(500, {"Content-Type": "text/plain"});
-            response.write(err + "\n");
+function getFile(pathname, args, request, response)
+{
+
+    fs.readFile(pathname, 'binary', function (err, file) {
+        if (err) {
+            response.writeHead(500, {'Content-Type': 'text/plain'});
+            response.write(err + '\n');
             response.end();
             return;
         }
-        
+
         response.writeHead(200);
-        response.write(file, "binary");
+        var html = applyTemplate(file.toString(), args);
+        response.write(html);
         response.end();
     });
 }
-console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+function getQueryStringArgs(query)
+{
+    var obj = {};
+    if (query){
+        query.split('&').forEach(function (pair) {
+            var arr = pair.split('=');
+            obj[arr[0]] = arr[1];
+        });
+    }
+
+    return obj;
+}
+
+function applyTemplate(html, args)
+{
+    for (var key in args){
+        html = html.replace('<!--% '+ key +' %-->', args[key]);
+    }
+
+    return html;
+}
+console.log('Static file server running at\n  => http://localhost:' + port + '/\nCTRL + C to shutdown');
